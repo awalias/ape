5c14f4f27b1d4a5334be0abc8ec4e75e12556124
Anthony  Wilson <anthony.wilson14@imperial.ac.uk>
2015-06-06 14:36:57 +0100

    experimentation

--- src/bg/background.js
+++ src/bg/background.js
@@ -4,10 +4,41 @@
 //     "sample_setting": "This is how you use Store.js to remember values"
 // });
 
-
 //example of using a message handler from the inject scripts
-chrome.extension.onMessage.addListener(
-  function(request, sender, sendResponse) {
-  	chrome.pageAction.show(sender.tab.id);
-    sendResponse();
-  });
+var requestFilter = {
+	urls: [
+		"<all_urls>"
+	]
+};
+
+chrome.webRequest.onBeforeSendHeaders.addListener(function(details) {
+	var headers = details.requestHeaders;
+	console.log("pimpin");
+		// if( !localStorage['user-agent'] ) {
+	// 	return;
+	// }
+	for(var i = 0, l = headers.length; i < l; ++i) {
+		if( headers[i].name == 'User-Agent' ) {
+			break;
+		}
+	}
+	if(i < headers.length) {
+		headers[i].value = "HALLO, VORLD!";//localStorage['user-agent'];
+	}
+	return {requestHeaders: headers};
+}, requestFilter, ['requestHeaders','blocking']);
+
+chrome.webRequest.onBeforeRequest.addListener(
+	function(details) { return {cancel: true}; },
+	{urls: ["*://www.evil.com/*"]},
+	["blocking"]);
+
+// (function() {
+//     var userAgent = window.navigator.userAgent;
+//     Object.defineProperty(window.navigator, "userAgent", {
+//         get: function() {
+//             console.log("hello from useragent");
+//             return userAgent;
+//         }
+//     });
+// })();
